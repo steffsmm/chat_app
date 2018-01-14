b29336c716d2adc009ad5d743388342d565c0f70
Стефани Мучинова <you@example.com>
2018-01-14 17:08:38 +0200

    socket setup

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
 const path = require('path');
-const publicPath = path.join(__dirname,"../public")
-const express    = require('express')
-const app = express()
-
-app.use(express.static(publicPath))
+const http = require('http');
+const publicPath = path.join(__dirname,"../public");
 const port = process.env.PORT || 3000;
+const express    = require('express');
+const socketIO = require('socket.io');
+const app = express()
+var server = http.createServer(app);
+var io = socketIO(server)
+app.use(express.static(publicPath))
 
 // app.get('/', function(req, res){
 //     res.send(`${publicPath}\\index.html`);
 // });
 
-app.listen(port, ()=>{
+io.on('connection', (socket)=>{
+  console.log("New user connected");
+
+  socket.on('disconnect',()=>{
+    console.log("User disconnected");
+  })
+})
+
+server.listen(port, ()=>{
   console.log(`Started on port ${port}`);
 });
