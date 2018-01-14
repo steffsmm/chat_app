f1b684c9d6e2f8d489c994412b14f56319fb0873
Стефани Мучинова <you@example.com>
2018-01-14 16:24:37 +0200

    fixed port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express    = require('express')
 const app = express()
 
 app.use(express.static(publicPath))
-const process.env.PORT port = 3000;
+const port = process.env.PORT || 3000;
 
 // app.get('/', function(req, res){
 //     res.send(`${publicPath}\\index.html`);
